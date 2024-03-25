4edb0e4e95d3aafbfa25081a55b82bc5914ddf6c
Eduarda Mesquita <126674559+eduardammag@users.noreply.github.com>
2024-03-25 08:01:32 -0300

    Questao1.cpp

--- atividade.cpp
+++ atividade.cpp
@@ -5,7 +5,31 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int main()
-{
+// QUESTÃO 1
+
+float raiz_quadrada(float num) {
+    float resultado = num / 2;
+    float temp;
+
+    do {
+        temp = resultado;
+        resultado = (temp + (num / temp)) / 2;
+    } while ((temp - resultado) != 0);
+
+    return resultado;
+}
+
+int main() {
+    float numero;
+    cout << "Digite um número para calcular a raiz quadrada: ";
+    cin >> numero;
+    
+    if (numero < 0) {
+        cout << "Não é possível calcular a raiz quadrada de um número negativo." << endl;
+    } else {
+        cout << "A raiz quadrada de " << numero << " é aproximadamente " << raiz_quadrada(numero) << endl;
+    }
+    
     return 0;
 }
+
